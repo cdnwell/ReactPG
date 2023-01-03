2a9a4c186869d55ab346d46e6071287db9490e4c
cdnwell <cdnwellhk@gmail.com>
2023-01-03 20:16:56 +0900

    commit first react-complete-guide study

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExportItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get Started</h2>
+      <ExportItem />
+    </div>
+  );
+}
+
+export default App;
