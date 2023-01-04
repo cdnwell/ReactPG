e5d14ec43b4da766b3100e347d2d28b4bae67bb0
cdnwell <cdnwellhk@gmail.com>
2023-01-04 20:20:39 +0900

    commit reactstudy

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
-import ExportItem from './components/ExpenseItem';
+import React from 'react';
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      title : 'Car Insurance costs',
+      amount : '$213.24',
+      date : new Date('2023-01-02 00:00:00')
+    },
+    {
+      title : 'Subway costs',
+      amount : '$10.21',
+      date : new Date(2022,12,12)
+    },
+    {
+      title : 'food buying',
+      amount : '$300.1',
+      date : new Date(2022,12,11)
+    },
+    {
+      title : 'building repairing',
+      amount : '$250',
+      date : new Date(2022,11,10)
+    }
+  ];
+
   return (
     <div>
       <h2>Let's get Started</h2>
-      <ExportItem />
+      <Expenses items={expenses}/>
     </div>
   );
+
+  // return React.createElement(
+  //   'div',
+  //   {},  // div의 속성을 의미한다. 속성이 없으므로 빈칸
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, {items : expenses}) // 안에 컨텐츠가 없으므로 세 번째 자리는 없어도 된다.
+  // );
 }
 
 export default App;
